c524ec1da19db23fed29642a39de56e0d10df608
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-09-25 09:30:12 +0530

    Lab 3 Program 1

---
+++ lab3/Program1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Time {
+    int hh, mm, ss;
+public:
+    void input(int h=0, int m=0, int s=0);
+    void show();
+};
+void Time::input(int h, int m, int s) {
+    hh = h;
+    mm = m;
+    ss = s;
+}
+void Time::show() {
+    cout << hh << ":" << mm << ":" << ss << endl;
+}
+int main() {
+    Time t1, t2;
+    int h, m, s;
+    cout << "Enter time 1 (hh mm ss): ";
+    cin >> h >> m >> s;
+    t1.input(h, m, s);
+    cout << "Time 1: ";
+    t1.show();
+    cout << "\nEnter time 2 (hh mm ss): ";
+    cin >> h >> m >> s;
+    t2.input(h, m, s);
+    cout << "Time 2: ";
+    t2.show();
+    return 0;
+}
