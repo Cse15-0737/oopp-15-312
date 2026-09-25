8f82763ef1a2f0acb36d94dc1bff6dce1fcbb1e7
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-09-25 09:39:22 +0530

    Lab 1 experiment 1

--- lab1/Experiment1.cpp
+++ lab1/Experiment1.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 using namespace std;
-
 class Student {
     int rollNo;
     string name;
     float marks;
-
 public:
     void getData() {
-        cout << "Enter Roll No: ";
-        cin >> rollNo;
-        cout << "Enter Name: ";
-        cin >> name;
-        cout << "Enter Marks: ";
-        cin >> marks;
+        cout<<"Enter Roll No: ";
+        cin>>rollNo;
+        cout<<"Enter Name: ";
+        cin>>name;
+        cout<<"Enter Marks: ";
+        cin>>marks;
     }
-
     void display() {
-        cout << "\nStudent Details\n";
-        cout << "Roll No: " << rollNo << endl;
-        cout << "Name: " << name << endl;
-        cout << "Marks: " << marks << endl;
+        cout <<"\nStudent Details\n";
+        cout <<"Roll No: "<< rollNo << endl;
+        cout <<"Name: "<< name << endl;
+        cout <<"Marks: "<< marks << endl;
     }
 };
-
 int main() {
     Student s;
     s.getData();
     s.display();
-
     return 0;
 }
